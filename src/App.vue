379ca52d410e4c580495f44ece0ff8f00d8f5428
derjogi <derjogi@web.de>
2022-02-07 11:54:22 +0100

    Adding draft sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,45 @@
 <template>
+    <sidebar-menu :menu="menu" />
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import { SidebarMenu } from 'vue-sidebar-menu'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    SidebarMenu
+  },
+  data() {
+    return {
+      menu: [
+        {
+          header: true,
+          title: 'Main Navigation',
+          hiddenOnCollapse: true
+        },
+        {
+          href: '/',
+          title: 'Dashboard',
+          icon: 'fa fa-user'
+        },
+        {
+          href: '/charts',
+          title: 'Charts',
+          icon: 'fa fa-chart-area',
+          child: [
+            {
+              href: '/charts/sublink',
+              title: 'Sub Link'
+            }
+          ]
+        }
+      ]
+    }
   }
 }
 </script>
